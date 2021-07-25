68381c54054c3248e417cc5ad93bdcfc66383ab3
Yannick Kabasele <Yanka@MacBook-Pro-de-Yannick.local>
2021-07-25 10:31:26 +0200

    first commit

---
+++ precache-manifest.b0d80187a3f0825f6b98e1d6408e7557.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29b56729a534d2dfbfe",
+    "url": "/css/app.a0855ce6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "650bb956bd7977740d83baf69bf36230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8a19c3217f782ee08b",
+    "url": "/js/about.8164b8de.js"
+  },
+  {
+    "revision": "c29b56729a534d2dfbfe",
+    "url": "/js/app.0045b3dc.js"
+  },
+  {
+    "revision": "3ef8a3f3c16c92ce10b9",
+    "url": "/js/chunk-vendors.bb2d815b.js"
+  },
+  {
+    "revision": "f330cd66cd929eebc26eb0f17f0ba63e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
